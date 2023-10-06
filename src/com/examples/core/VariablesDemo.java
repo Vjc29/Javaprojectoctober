334b26ce8166a5be436ff6e4289d638aa3b2fcf7
jayanthchowdary <jayanth29v@gmail.com>
2023-10-06 13:26:41 -0500

    added variable demo class

---
+++ src/com/examples/core/VariablesDemo.java
@@ -0,0 +1,25 @@
+package com.examples.core;
+
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 25, secondNumber = 50;
+        float result= firstNumber + secondNumber;
+
+        System.out.println("result: " + result);
+
+        result= firstNumber - secondNumber;
+
+        System.out.println("result: " + result);
+
+        result= firstNumber * secondNumber;
+
+        System.out.println("result: " + result);
+
+        result= (float) firstNumber / secondNumber;
+
+        System.out.println("result: " + result);
+
+    }
+}
