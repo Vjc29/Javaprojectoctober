ce04fc44c0ef6c8b0c754c2c127d2e71ce3972a5
jayanthchowdary <jayanth29v@gmail.com>
2023-10-09 13:07:56 -0500

    Added Conditions

---
+++ src/com/examples/core/ConditionsDemo.java
@@ -0,0 +1,15 @@
+package com.examples.core;
+
+public class ConditionsDemo {
+    public static void main(String[] args) {
+        boolean flag=true;
+
+        if(flag==true)
+        {
+            System.out.println("True Condition");
+        }
+        else {
+            System.out.println("False Condition");
+        }
+    }
+}
